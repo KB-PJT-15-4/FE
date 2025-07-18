e07f4d1e3eae964be0484d616ad4e760eed34518
Ian <105128049+minijae011030@users.noreply.github.com>
2025-07-18 11:13:21 +0900

    Chore: tailwind config 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: '14px',
+        md: '10px',
+        sm: '6px',
+      },
+      colors: {
+        'moa-main': '#87BFFF',
+        'moa-disabled': '#CCCCCC',
+        'moa-error': '#DB0000',
+        'moa-success': '#009F2C',
+        'moa-main-text': '#3E3E3E',
+        'moa-sub-text': '#949494',
+      },
+    },
   },
   plugins: [],
-};
+}
