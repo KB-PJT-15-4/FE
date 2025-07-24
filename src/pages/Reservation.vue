88bf15467f050f5baff03d8926f4e23514da6525
Ian <105128049+minijae011030@users.noreply.github.com>
2025-07-24 13:35:45 +0900

    Feat: type에 따른 옵션 렌더링

--- src/pages/Reservation.vue
+++ src/pages/Reservation.vue
@@ -2,6 +2,43 @@
   <div class="flex w-full flex-col gap-3">
     <TypographyHead3>예매하기</TypographyHead3>
     <ItemInfo :item="item" />
+
+    <!-- 숙박 -->
+    <div v-if="type === ItemType.Accommodation">
+      <TypographySubTitle1>
+        입실일: {{ (reservationInfo as AccommodationReservation).startDate }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>
+        퇴실일: {{ (reservationInfo as AccommodationReservation).endDate }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>인원: {{ reservationInfo.people }}명</TypographySubTitle1>
+    </div>
+
+    <!-- 교통 -->
+    <div v-if="type === ItemType.Transportation">
+      <TypographySubTitle1>
+        출발지: {{ (reservationInfo as TransportationReservation).origin }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>
+        도착지:
+        {{ (reservationInfo as TransportationReservation).destination }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>
+        출발일: {{ (reservationInfo as TransportationReservation).date }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>인원: {{ reservationInfo.people }}명</TypographySubTitle1>
+    </div>
+
+    <!-- 식당 -->
+    <div v-if="type === ItemType.Restaurant">
+      <TypographySubTitle1>
+        카테고리: {{ (reservationInfo as RestaurantReservation).category }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>
+        예약일: {{ (reservationInfo as RestaurantReservation).date }}
+      </TypographySubTitle1>
+      <TypographySubTitle1>인원: {{ reservationInfo.people }}명</TypographySubTitle1>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
@@ -14,6 +51,7 @@ import {
 import { reservationItemInfoMockData } from '@/entities/trip/trip.mock'
 import ItemInfo from '@/features/trip/Reservation/ui/ItemInfo.vue'
 import TypographyHead3 from '@/shared/components/atoms/typography/TypographyHead3.vue'
+import TypographySubTitle1 from '@/shared/components/atoms/typography/TypographySubTitle1.vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -21,7 +59,7 @@ const route = useRoute()
 const tripId = route.params.tripId as string
 console.log(tripId)
 
-const type = route.query.type as string
+const type = route.query.type as ItemType
 const itemId = route.query.itemId as string
 const item = reservationItemInfoMockData // 추후 itemId, type을 통해 받아올 예정
 
@@ -46,7 +84,7 @@ if (type === ItemType.Transportation) {
   }
 }
 
-if (type === ItemType.Transportation) {
+if (type === ItemType.Restaurant) {
   reservationInfo = {
     itemId,
     people: Number(route.query.people),
