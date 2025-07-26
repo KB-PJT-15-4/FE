b0018e2f9ca0874df26821aed65244dcaccb6cb4
Ian <105128049+minijae011030@users.noreply.github.com>
2025-07-26 11:35:46 +0900

    Feat: notification feature 분리

---
+++ src/features/user/Notification/ui/NotificationCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <Card
+    :key="notification.id"
+    class="my-4"
+  >
+    <div v-if="notification.type === NotificationTye.SETTLE">
+      <div class="flex gap-3">
+        <i class="bi bi-wallet" />
+        <TypographyP1>
+          {{ notification.sender }} 님이 {{ notification.tripName }} 여행의 정산을 요청하였습니다.
+        </TypographyP1>
+      </div>
+      <div class="flex w-full justify-end gap-2">
+        <ButtonExtraSmallMain>이동</ButtonExtraSmallMain>
+      </div>
+    </div>
+
+    <div v-if="notification.type === NotificationTye.TRIP">
+      <div class="flex gap-3">
+        <i class="bi bi-airplane" />
+        <TypographyP1>
+          {{ notification.sender }} 님이 {{ notification.tripName }} 여행에 초대하였습니다.
+        </TypographyP1>
+      </div>
+      <div class="flex w-full justify-end gap-2">
+        <ButtonExtraSmallMain>수락</ButtonExtraSmallMain>
+        <ButtonExtraSmallSub>거절</ButtonExtraSmallSub>
+      </div>
+    </div>
+  </Card>
+</template>
+<script setup lang="ts">
+import { NotificationTye, type UserNotification } from '@/entities/user/user.entity'
+import ButtonExtraSmallMain from '@/shared/components/atoms/button/ButtonExtraSmallMain.vue'
+import ButtonExtraSmallSub from '@/shared/components/atoms/button/ButtonExtraSmallSub.vue'
+import Card from '@/shared/components/atoms/card/Card.vue'
+import TypographyP1 from '@/shared/components/atoms/typography/TypographyP1.vue'
+
+defineProps<{ notification: UserNotification }>()
+</script>
--- src/pages/home/Notification.vue
+++ src/pages/home/Notification.vue
@@ -9,47 +9,19 @@
         {{ item }}
       </Option>
     </SelectSmall>
-    <Card
+    <div
       v-for="(item, index) in userNotificationMockData"
       :key="index"
-      class="my-4"
     >
-      <div v-if="item.type === NotificationTye.SETTLE">
-        <div class="flex gap-3">
-          <i class="bi bi-wallet" />
-          <TypographyP1>
-            {{ item.sender }} 님이 {{ item.tripName }} 여행의 정산을 요청하였습니다.
-          </TypographyP1>
-        </div>
-        <div class="flex w-full justify-end gap-2">
-          <ButtonExtraSmallMain>이동</ButtonExtraSmallMain>
-        </div>
-      </div>
-
-      <div v-if="item.type === NotificationTye.TRIP">
-        <div class="flex gap-3">
-          <i class="bi bi-airplane" />
-          <TypographyP1>
-            {{ item.sender }} 님이 {{ item.tripName }} 여행에 초대하였습니다.
-          </TypographyP1>
-        </div>
-        <div class="flex w-full justify-end gap-2">
-          <ButtonExtraSmallMain>수락</ButtonExtraSmallMain>
-          <ButtonExtraSmallSub>거절</ButtonExtraSmallSub>
-        </div>
-      </div>
-    </Card>
+      <NotificationCard :notification="item" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
-import { NotificationTye } from '@/entities/user/user.entity'
 import { userNotificationMockData } from '@/entities/user/user.mock'
-import ButtonExtraSmallMain from '@/shared/components/atoms/button/ButtonExtraSmallMain.vue'
-import ButtonExtraSmallSub from '@/shared/components/atoms/button/ButtonExtraSmallSub.vue'
-import Card from '@/shared/components/atoms/card/Card.vue'
+import NotificationCard from '@/features/user/Notification/ui/NotificationCard.vue'
 import Option from '@/shared/components/atoms/input/Option.vue'
 import SelectSmall from '@/shared/components/atoms/input/SelectSmall.vue'
-import TypographyP1 from '@/shared/components/atoms/typography/TypographyP1.vue'
 import { ref } from 'vue'
 
 const options = ['전체', '정산요청', '여행초대']
