f462899f61b276dca0bca2e21b1e38c05db3dad0
Ian <105128049+minijae011030@users.noreply.github.com>
2025-07-31 09:33:09 +0900

    Feat: reservation page

--- src/entities/trip/trip.entity.ts
+++ src/entities/trip/trip.entity.ts
@@ -22,6 +22,28 @@ export const restaurantTypeList = [
 ]
 export const timeOptions = ['07:30', '10:00', '12:30', '14:00', '15:30', '18:00', '19:30', '21:00']
 export const containers = ['1칸', '2칸', '3칸', '4칸', '5칸', '6칸', '7칸', '8칸', '9칸', '10칸']
+export const reservationTime = [
+  '10:00',
+  '10:30',
+  '11:00',
+  '11:30',
+  '12:00',
+  '12:30',
+  '13:00',
+  '13:30',
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+  '18:00',
+  '18:30',
+  '19:00',
+  '19:30',
+]
 
 export enum ItemType {
   Accommodation = 'accommodation',
