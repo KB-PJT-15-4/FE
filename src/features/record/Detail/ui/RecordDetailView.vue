ee3717ac428b033c0ad2429120cec2046405e61b
REVE97 <zxcv9675@naver.com>
2025-08-05 10:39:43 +0900

    Feat: RecordDetailCustom.vue -> 해당 기록 상세보기 페이지 추가

---
+++ src/features/record/Detail/ui/RecordDetailView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="fixed top-0 left-0 w-full h-full bg-black bg-opacity-30 z-50 flex justify-center items-center"
+  >
+    <Card class="bg-white max-w-[500px] max-h-[80vh] overflow-y-auto m-4">
+      <div class="w-full flex justify-end mb-4">
+        <button @click="emit('close')">
+          <i class="bi bi-x text-[30px]" />
+        </button>
+      </div>
+      <div class="flex flex-col gap-4 p-2">
+        <div class="space-y-4">
+          <div>
+            <TypographyHead2>{{ record?.title }}</TypographyHead2>
+          </div>
+          <div>
+            <TypographySubTitle2 class="text-[#626262]">
+              {{ formatFullDateToKorean(new Date(record?.date || '')) }}
+            </TypographySubTitle2>
+          </div>
+          <div
+            v-if="record?.imageUrl"
+            class="flex justify-center"
+          >
+            <img
+              :src="record.imageUrl"
+              class="max-w-full h-auto rounded-md shadow-sm"
+              alt="기록 이미지"
+            >
+          </div>
+          <div class="bg-gray-50 p-4 rounded-md">
+            <TypographyP2 class="whitespace-pre-line leading-relaxed">
+              {{ record?.content }}
+            </TypographyP2>
+          </div>
+        </div>
+      </div>
+    </Card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps } from 'vue'
+import { formatFullDateToKorean } from '@/shared/utils/format'
+
+import Card from '@/shared/components/atoms/card/Card.vue'
+import TypographyHead2 from '@/shared/components/atoms/typography/TypographyHead2.vue'
+import TypographySubTitle2 from '@/shared/components/atoms/typography/TypographySubTitle2.vue'
+import TypographyP2 from '@/shared/components/atoms/typography/TypographyP2.vue'
+
+import type { RecordProps } from '@/entities/record/record.entity'
+
+defineProps<RecordProps>()
+
+const emit = defineEmits(['close'])
+</script>
