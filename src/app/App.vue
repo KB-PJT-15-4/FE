0dbf56a4b1a4c00940b9090ed61427afffac265b
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-06 17:44:18 +0900

    Chore: fcm 초기설정

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
+import { initFCM } from '@/initFCM'
 import AppLayout from '@/layout/AppLayout.vue'
 import { RouterView } from 'vue-router'
+
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
+    initFCM()
+  } else {
+    console.warn('알림 권한 거부됨')
+  }
+})
 </script>
 
 <template>
