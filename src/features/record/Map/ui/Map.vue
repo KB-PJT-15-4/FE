4c2b05b0d95f331dcf092299573731857a733dc6
REVE97 <zxcv9675@naver.com>
2025-08-13 11:10:35 +0900

    Refactor: Map.vue -> service 기능분리, 오버레이 투명도 조절 / RecordTripCard.vue -> service 기능분리

---
+++ src/features/record/Map/ui/Map.vue
@@ -0,0 +1,89 @@
+<template>
+  <div
+    id="map"
+    class="w-full h-[450px] rounded-md"
+  />
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import {
+  ensureKakaoLoaded,
+  fetchLocationsService,
+  getIconSrcByLocationName,
+  createCircleMarker,
+  createInfoBalloon,
+} from '../services/map.service'
+
+const emit = defineEmits(['selectLocation'])
+const kakaoMapKey = import.meta.env.VITE_KAKAOMAP_KEY // 카카오맵 API 호출키
+const apiBaseUrl = import.meta.env.VITE_APP_API_URL // localhost:8080 대체
+
+const locations = ref([])
+
+onMounted(async () => {
+  try {
+    const token = localStorage.getItem('accessToken')
+    locations.value = await fetchLocationsService({
+      token,
+      apiBaseUrl,
+    })
+
+    await ensureKakaoLoaded(kakaoMapKey)
+
+    initMap()
+  } catch (error) {
+    console.error('지도 초기화 중 오류:', error)
+  }
+})
+
+function initMap() {
+  window.kakao.maps.load(() => {
+    const container = document.getElementById('map')
+    if (!container) return
+
+    const options = {
+      center: new window.kakao.maps.LatLng(36.35041, 127.38455),
+      level: 15,
+    }
+
+    const map = new window.kakao.maps.Map(container, options)
+
+    const zoomControl = new window.kakao.maps.ZoomControl()
+    map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT)
+
+    const infoOverlay = new window.kakao.maps.CustomOverlay({ yAnchor: 1 })
+
+    locations.value.forEach((item) => {
+      const position = new window.kakao.maps.LatLng(item.latitude, item.longitude)
+      const iconSrc = getIconSrcByLocationName(item.locationName)
+      const markerContent = createCircleMarker(iconSrc, 70) // 원형 이미지 마커
+
+      const markerOverlay = new window.kakao.maps.CustomOverlay({
+        position,
+        content: markerContent,
+        yAnchor: 1,
+      })
+      markerOverlay.setMap(map)
+
+      markerContent.addEventListener('click', () => {
+        emit('selectLocation', item.locationName)
+
+        const balloon = createInfoBalloon(item)
+        infoOverlay.setContent(balloon)
+        infoOverlay.setPosition(position)
+        infoOverlay.setMap(map)
+
+        const closeButton = balloon.querySelector('#closeOverlay')
+        if (closeButton) {
+          closeButton.addEventListener('click', () => {
+            infoOverlay.setMap(null)
+          })
+        }
+      })
+    })
+  })
+}
+</script>
+
+
