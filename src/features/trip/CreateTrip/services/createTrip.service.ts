eb608611d53b7e4787b272ba95f7ba537e397f07
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-14 11:59:55 +0900

    Refactor: create trip 서비스 주석 추가

--- src/features/trip/CreateTrip/services/createTrip.service.ts
+++ src/features/trip/CreateTrip/services/createTrip.service.ts
@@ -1,6 +1,15 @@
 import { api } from '@/shared/utils/api'
 import { API_END_POINT, type ApiData } from '@/shared/utils/fetcher'
 
+/**
+ * 여행 생성
+ * @param tripName 여행 이름
+ * @param startTime 여행 시작 일자
+ * @param endTime 여행 종료 일자
+ * @param location 여행 지역
+ * @param memberIds 여행 멤버 리스트
+ * @returns trip id
+ */
 export async function createTrip(
   tripName: string,
   startTime: string,
@@ -24,6 +33,11 @@ export async function createTrip(
   return res.data
 }
 
+/**
+ * 여행 멤버 email을 통해 id 반환
+ * @param email 여행 멤버 이메일
+ * @returns 멤버 id
+ */
 export async function getIdByEmail(email: string): Promise<number> {
   const { url, method } = API_END_POINT.user.getIdByEmail(email)
 
@@ -31,6 +45,12 @@ export async function getIdByEmail(email: string): Promise<number> {
   return res.data
 }
 
+/**
+ * 여행 멤버 email을 통해 이미 여행에 있는 멤버인지 검사하고 id를 반환
+ * @param email 여행 멤버 이메일
+ * @param tripId 여행 id
+ * @returns 멤버 id
+ */
 export async function getValidMemberIdByEmail(email: string, tripId: string): Promise<number> {
   const { url, method } = API_END_POINT.user.getValidMemberIdByEmail(email, tripId)
 
@@ -38,6 +58,12 @@ export async function getValidMemberIdByEmail(email: string, tripId: string): Pr
   return res.data
 }
 
+/**
+ * 여행에 멤버 초대
+ * @param tripId 여행 id
+ * @param memberIds 초대하려는 멤버 id 리스트
+ * @returns 초대 성공 여부
+ */
 export async function inviteMembers(tripId: number, memberIds: number[]): Promise<boolean> {
   const { url, method } = API_END_POINT.trip.inviteMembers()
 
