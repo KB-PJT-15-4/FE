c4d189b993be709405db4c4c98e03b500979a939
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-14 10:51:06 +0900

    Refactor: my reservation list 서비스 주석 추가

--- src/features/trip/MyReservationList/services/myReservationList.service.ts
+++ src/features/trip/MyReservationList/services/myReservationList.service.ts
@@ -2,11 +2,19 @@ import type { Reservation, TripInfo, UserReservationList } from '@/entities/trip
 import { api } from '@/shared/utils/api'
 import { API_END_POINT, type ApiData, type Paged } from '@/shared/utils/fetcher'
 
+/**
+ * 나의 예약 내역 조회
+ * @param tripId 여행 id
+ * @param page 페이지네이션 page
+ * @param size 페이지네이션 size
+ * @param resKind 조회하려는 업종
+ * @returns UserReservationList
+ */
 export async function getMyReservationList(
   tripId: number,
   page: number,
   size: number,
-  resKind: string | null
+  resKind: 'TRANSPORT' | 'ACCOMMODATION' | 'RESTAURANT' | null
 ): Promise<Paged<UserReservationList>> {
   const { url, method } = API_END_POINT.trip.getReservationList(tripId, resKind, page, size)
 
@@ -14,6 +22,11 @@ export async function getMyReservationList(
   return res.data
 }
 
+/**
+ * 예약 내역 QR 및 정보 조회
+ * @param itemId 예약 id
+ * @returns Reservation
+ */
 export async function getReservationQR(itemId: number): Promise<Reservation[]> {
   const { url, method } = API_END_POINT.trip.getReservationQr(itemId)
 
@@ -21,6 +34,11 @@ export async function getReservationQR(itemId: number): Promise<Reservation[]> {
   return res.data
 }
 
+/**
+ * 여행의 정보를 조회
+ * @param tripId 여행 id
+ * @returns TripInfo
+ */
 export async function getTripInfo(tripId: string): Promise<TripInfo> {
   const { url, method } = API_END_POINT.trip.getTripInfo(tripId)
 
