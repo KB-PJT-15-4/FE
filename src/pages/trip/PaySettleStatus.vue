5e40d96195e5d0c198fe4fec828f8e7343e74a14
Ian <105128049+minijae011030@users.noreply.github.com>
2025-08-19 09:43:05 +0900

    Design: settlement status 화면 버튼 수정
    화면 하단에 붙으면서 스크롤이 생기지 않도록

--- src/pages/trip/PaySettleStatus.vue
+++ src/pages/trip/PaySettleStatus.vue
@@ -1,44 +1,47 @@
 <template>
   <div
     v-if="data"
-    class="w-full flex flex-col gap-3"
+    class="w-full flex flex-col gap-3 h-full pb-7 justify-between"
   >
-    <div>
-      <TypographyHead2>{{ data.expenseName }}</TypographyHead2>
-      <TypographySubTitle2 class="text-moa-sub-text">
-        {{ formatFullDateToKorean(new Date(data.expenseDate)) }}
-      </TypographySubTitle2>
-    </div>
+    <div class="flex flex-col gap-3">
+      <div>
+        <TypographyHead2>{{ data.expenseName }}</TypographyHead2>
+        <TypographySubTitle2 class="text-moa-sub-text">
+          {{ formatFullDateToKorean(new Date(data.expenseDate)) }}
+        </TypographySubTitle2>
+      </div>
 
-    <div>
-      <TypographyHead1>총 {{ formatNumber(data.amount) }} 원</TypographyHead1>
-      <TypographySubTitle2 class="text-moa-sub-text">
-        1인당 {{ formatNumber(data.amount / data.progresses.length) }} 원
-      </TypographySubTitle2>
-    </div>
-    <TypographyHead2>정산 현황</TypographyHead2>
-    <div class="flex flex-col gap-3 h-[430px]">
-      <Card
-        v-for="(item, index) in data.progresses"
-        :key="index"
-        class="flex justify-between items-center py-4"
-      >
-        <TypographyHead3>{{ item.name }}</TypographyHead3>
-        <div>
-          <div v-if="item.status == '정산 진행중'">
-            <Tag color="main">
-              <TypographyCaption>정산진행중</TypographyCaption>
-            </Tag>
+      <div>
+        <TypographyHead1>총 {{ formatNumber(data.amount) }} 원</TypographyHead1>
+        <TypographySubTitle2 class="text-moa-sub-text">
+          1인당 {{ formatNumber(data.amount / data.progresses.length) }} 원
+        </TypographySubTitle2>
+      </div>
+      <TypographyHead2>정산 현황</TypographyHead2>
+      <div class="flex flex-col gap-3">
+        <Card
+          v-for="(item, index) in data.progresses"
+          :key="index"
+          class="flex justify-between items-center py-4"
+        >
+          <TypographyHead3>{{ item.name }}</TypographyHead3>
+          <div>
+            <div v-if="item.status == '정산 진행중'">
+              <Tag color="main">
+                <TypographyCaption>정산진행중</TypographyCaption>
+              </Tag>
+            </div>
+            <div v-if="item.status == '정산 완료'">
+              <Tag color="pink">
+                <TypographyCaption>정산완료</TypographyCaption>
+              </Tag>
+            </div>
           </div>
-          <div v-if="item.status == '정산 완료'">
-            <Tag color="pink">
-              <TypographyCaption>정산완료</TypographyCaption>
-            </Tag>
-          </div>
-        </div>
-      </Card>
+        </Card>
+      </div>
     </div>
     <ButtonMain
+      class=""
       @click="
         router.replace({
           name: 'trip_detail',
